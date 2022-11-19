ec612cba10eeb49b6fd5b7ad7ede13af819119b5
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-11-19 11:14:03 -0800

    add button and state updating

--- app/src/main/java/com/example/basicstateandroid/WaterCounter.kt
+++ app/src/main/java/com/example/basicstateandroid/WaterCounter.kt
@@ -1,13 +1,25 @@
 package com.example.basicstateandroid
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(text = "You've had $count glasses.", modifier = modifier.padding(16.dp))
+    val count: MutableState<Int> = remember { mutableStateOf(0) }
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "You've had ${count.value} glasses."
+        )
+        Button(onClick = { count.value++ }) {
+            Text(text = "Add one")
+        }
+    }
 }
