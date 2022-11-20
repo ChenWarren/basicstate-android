4e033a3e8087f1c842076dbd4ac54c37f18c8f3c
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-11-19 21:21:27 -0800

    add state

--- app/src/main/java/com/example/basicstateandroid/WaterCounter.kt
+++ app/src/main/java/com/example/basicstateandroid/WaterCounter.kt
@@ -1,25 +1,31 @@
 package com.example.basicstateandroid
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count: MutableState<Int> = remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
     Column(modifier = modifier.padding(16.dp)) {
-        Text(
-            text = "You've had ${count.value} glasses."
-        )
-        Button(onClick = { count.value++ }) {
-            Text(text = "Add one")
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Row(
+            Modifier.padding(top = 8.dp)
+        ) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
         }
     }
 }
